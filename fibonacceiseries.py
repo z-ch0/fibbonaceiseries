834a85acdf2d5c6c454a9d05acb844ce9c473e99
z-ch0 <shlokd043@gmail.com>
2025-05-15 15:41:45 +0530

    Update fibonacceiseries.py
    By Shlok Dwivedi

--- fibonacceiseries.py
+++ fibonacceiseries.py
@@ -1,7 +1,7 @@
-def fibo(n):
+def fibon(n):
     a, b = 0, 1
-    for _ in range(n):
+    for i in range(n):
         yield a
         a, b = b, a + b
 n = 10
-print(list(fibo(n)))
+print(list(fibon(n)))
